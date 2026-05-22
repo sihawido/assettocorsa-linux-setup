e536cc2572e0c24be0b594f5c4f95cce0197c761
siha <sihawido@gmail.com>
2026-05-22 23:19:39 +0100

    add artix support

--- assettocorsa-linux-setup.sh
+++ assettocorsa-linux-setup.sh
@@ -89,7 +89,7 @@ required_packages=("wget" "tar" "unzip" "glib2" "protontricks")
 # Supported distros
 supported_apt=("debian" "ubuntu" "linuxmint" "pop" "zorin")
 supported_dnf=("fedora" "nobara" "ultramarine")
-supported_arch=("arch" "endeavouros" "steamos" "cachyos")
+supported_arch=("arch" "endeavouros" "steamos" "cachyos" "artix")
 supported_opensuse=("opensuse-tumbleweed")
 supported_slackware=("slackware" "salix")
 supported_gentoo=("gentoo")
