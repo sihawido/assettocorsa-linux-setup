9321ec327f9b8c3e3baf65e94cbb0b41dffda80f
siha <sihawido@gmail.com>
2026-06-14 17:14:14 +0100

    add support for KDE Neon

--- assettocorsa-linux-setup.sh
+++ assettocorsa-linux-setup.sh
@@ -87,7 +87,7 @@ function is-set {
 required_packages=("wget" "tar" "unzip" "glib2" "protontricks")
 
 # Supported distros
-supported_apt=("debian" "ubuntu" "linuxmint" "pop" "zorin")
+supported_apt=("debian" "ubuntu" "linuxmint" "pop" "zorin" "neon")
 supported_dnf=("fedora" "nobara" "ultramarine")
 supported_arch=("arch" "endeavouros" "steamos" "cachyos" "artix")
 supported_opensuse=("opensuse-tumbleweed")
